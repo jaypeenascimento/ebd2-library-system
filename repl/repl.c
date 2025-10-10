e26e3ea4dc403001820b2e9557e53a76e00da4c8
snjaypee-endeavouros <sn.jaypee@gmail.com>
2025-10-10 20:36:11 -0300

    feat: add REPL module

--- repl/repl.c
+++ repl/repl.c
@@ -1,47 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printCabecalho(){
-    printf("\nEscolha uma das opções abaixo:\n");
-    printf("\n[1] Carregar Categorias\n");
-    printf("[2] Listar Categorias\n"); 
-    printf("[3] Selecionar Categoria\n");
-    printf("[4] Mostrar Top 1 livro mais vendido\n");
-    printf("[5] Listar Top N livros mais vendidos\n");
-    printf("[6] Listar registrar venda:\n");
-    printf("[0] Sair\n");
-}   
+void printHeader() {
+  printf("\n");
 
-void chooseOption(int option){
-    switch(option){
-        case 1:
-            load_categories();
-            break;
-        case 2:
-            list_categories();
-            break;
-        case 3:
-            select_category();
-            break;
-        case 4:
-            top_1_book();
-            break;
-        case 5:
-            int n;
-            
-            printf("Digite o valor de N: ");
-            scanf("%d", &n);
-            
-            top_n_books(n);
+  printf("Escolha uma das opções abaixo:\n");
+  printf("[1] Carregar Categorias\n");
+  printf("[2] Listar Categorias\n");
+  printf("[3] Selecionar Categoria\n");
+  printf("[4] Mostrar Top 1 livro mais vendido\n");
+  printf("[5] Listar Top N livros mais vendidos\n");
+  printf("[6] Listar registrar venda:\n");
+  printf("[0] Sair\n");
+}
 
-            break;
-        case 6:
-            register_sale();
-            break;
-        case 0:
-            printf("Saindo...\n");
-            exit(0);
-        default:
-            printf("Opção inválida. Tente novamente.\n");
-    }
+void process(int option) {
+  switch (option) {
+  case 1:
+    break;
+  case 2:
+    break;
+  case 3:
+    break;
+  case 4:
+    break;
+  case 5:
+    // int n;
+    //
+    // printf("Digite o valor de N: ");
+    // scanf("%d", &n);
+    //
+
+    break;
+  case 6:
+    break;
+  case 0:
+    printf("Saindo...\n");
+    exit(0);
+  default:
+    printf("Opção inválida. Tente novamente.\n");
+  }
+}
+
+int read() {
+  int n = 0;
+  scanf("%d", &n);
+
+  return n;
+}
+
+void REPL() {
+  while (1) {
+    printHeader();
+    int input = read();
+    process(input);
+  }
 }
--- repl/repl.h
+++ repl/repl.h
@@ -1,4 +1,11 @@
 #ifndef REPL_H
 #define REPL_H
 
+struct REPL {
+  // ABB struct
+  // HEAP struct
+};
+
+void REPL();
+
 #endif

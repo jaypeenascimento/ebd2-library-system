416d88c58cb3c4a52c16f284b3294cff81c3f136
snjaypee-endeavouros <sn.jaypee@gmail.com>
2025-10-10 23:25:19 -0300

    feat: add heap.h

--- structures/heap.h
+++ structures/heap.h
@@ -1,4 +1,23 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+#include "../data.h"
+
+typedef struct Heap {
+  int index;
+  Book *book;
+
+  struct Heap *left;
+  struct Heap *right;
+} Heap;
+
+// Node
+Heap *heap_create_node(Book *book);
+void heap_destroy_node();
+
+// Heap
+void heap_insert(Heap *root, Heap *node);
+void heap_delete(Heap *root, Heap *node);
+void heap_destroy();
+
 #endif

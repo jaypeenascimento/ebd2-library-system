77d4922f2accba828d669833f067378002877446
snjaypee-endeavouros <sn.jaypee@gmail.com>
2025-10-11 21:21:47 -0300

    feat: add load-books

--- commands/commands.h
+++ commands/commands.h
@@ -1,18 +1,20 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+#include "../structures/heap.h"
 // protótipos das funções
 
-void load_categories(); //função para carregar categorias
+void load_categories(); // função para carregar categorias
 
-void list_categories(); //função para listar categorias
+void list_categories(); // função para listar categorias
 
-void select_category(); //função para selecionar categoria
+void load_books(Heap *heap,
+                char *filename); // função para selecionar categoria
 
-void top_1_book(); //função para mostrar o top 1 livro mais vendido
+void top_1_book(); // função para mostrar o top 1 livro mais vendido
 
-void top_n_books(int n); //função para mostrar o top N livros mais vendidos
+void top_n_books(int n); // função para mostrar o top N livros mais vendidos
 
-void register_sale(); //função para registrar uma venda
+void register_sale(); // função para registrar uma venda
 
 #endif /* COMMANDS_H */
--- commands/load-books.c
+++ commands/load-books.c
@@ -1,1 +1,32 @@
+#include "../structures/heap.h"
 #include "commands/commands.h"
+
+#include "./csv/csvReader.h"
+#include <stdio.h>
+
+Book **read_books(char *path) {
+  Book **books = ReadBooksFromCategory(path);
+  if (books == NULL) {
+    printf("Failed to load books from selected category: Cannot read books "
+           "from category provided: \"%s\"",
+           path);
+    return NULL;
+  }
+
+  return books;
+}
+
+void load_books(Heap *heap, char *filename) {
+  Book **books = read_books(filename);
+  if (!books)
+    return;
+
+  for (int i = 0; books[i] != NULL; i++) {
+    Book *curr = books[i];
+
+    Heap *node = heap_create_node(curr);
+    heap_insert(&heap, node);
+
+    printBook(curr);
+  }
+}
